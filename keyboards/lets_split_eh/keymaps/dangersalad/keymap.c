2aa7384e9e25d58b7e22d91d8aed323d3be46d14
paulbdavis <paul@dangersalad.com>
2018-03-06 00:49:31 -0700

    dangersalad 4x12 ortho

---
+++ keyboards/lets_split_eh/keymaps/dangersalad/keymap.c
@@ -0,0 +1,10 @@
+#include "lets_split_eh.h"
+#include "dangersalad.h"
+#include "action_layer.h"
+#include "eeconfig.h"
+
+extern keymap_config_t keymap_config;
+
+bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
+  return true;
+}
