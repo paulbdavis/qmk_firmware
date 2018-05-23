afacc374f9c58a72c7de92fced02249949cd5bfa
paulbdavis <paul@dangersalad.com>
2018-05-23 15:17:12 -0600

    tweaks

--- keyboards/lets_split/keymaps/dangersalad/config.h
+++ keyboards/lets_split/keymaps/dangersalad/config.h
@@ -33,6 +33,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
+#define PREVENT_STUCK_MODIFIERS
+
 #define TAPPING_TERM 200
 
 #endif
