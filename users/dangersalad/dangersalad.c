90ef3d30ed7af3678ddc76a35c84291758cd13f3
paulbdavis <paul@dangersalad.com>
2018-08-03 19:17:31 -0600

    more emacs

--- users/dangersalad/dangersalad.c
+++ users/dangersalad/dangersalad.c
@@ -67,14 +67,6 @@ typedef struct {
   int state;
 } tap;
 
-void pre_numpad_disable (void) {
-  // always deactivate these to prevent them getting stuck on since
-  // the numpad overrides them
-  layer_off(_RAISE);
-  unregister_code(KC_LALT);
-  unregister_code(KC_LGUI);
-}
-
 int cur_dance (qk_tap_dance_state_t *state) {
   if (state->count == 1) {
     //If count = 1, and it has been interrupted - it doesn't matter if it is pressed or not: Send SINGLE_TAP
@@ -112,6 +104,16 @@ static tap numpad_tap_state = {
   .is_press_action = true,
   .state = 0
 };
+
+void pre_numpad_disable (void) {
+  // always deactivate these to prevent them getting stuck on since
+  // the numpad overrides them
+  layer_off(_RAISE);
+  unregister_code(KC_LALT);
+  unregister_code(KC_LGUI);
+  alt_tap_state.is_press_action = true;
+  alt_tap_state.state = 0;
+}
 
 void ctrl_finished (qk_tap_dance_state_t *state, void *user_data) {
   ctrl_tap_state.state = cur_dance(state);
@@ -406,10 +408,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------'  `-----------------------------------------'
  */
 [_EMACS] =  LAYOUT_ortho_4x12( \
-  RESET          , EMACS_WIN_1       , EMACS_WIN_2     , EMACS_WIN_3       , EMACS_WIN_4        , XXXXXXX          , XXXXXXX          , XXXXXXX         , XXXXXXX         , XXXXXXX         , EMACS_WIN_0 , SSH_PUB , \
-  EMACS_PROJ_GIT , EMACS_PROJ_SWITCH , EMACS_PROJ_FILE , EMACS_PROJ_SEARCH , EMACS_PROJ_COMPILE , EMACS_PROJ_SHELL , EMACS_FLYC_CHECK , EMACS_FLYC_NEXT , EMACS_FLYC_PREV , EMACS_FLYC_LIST , XXXXXXX     , XXXXXXX , \
-  XXXXXXX        , XXXXXXX           , XXXXXXX         , XXXXXXX           , XXXXXXX            , XXXXXXX          , XXXXXXX          , XXXXXXX         , XXXXXXX         , XXXXXXX         , XXXXXXX     , XXXXXXX , \
-  _______        , _______           , XXXXXXX         , XXXXXXX           , _______            , XXXXXXX          , XXXXXXX          , _______         , XXXXXXX         , XXXXXXX         , XXXXXXX     , XXXXXXX \
+  RESET          , EMACS_WIN_1       , EMACS_WIN_2     , EMACS_WIN_3       , EMACS_WIN_4         , XXXXXXX          , XXXXXXX          , XXXXXXX         , XXXXXXX         , XXXXXXX         , EMACS_WIN_0 , SSH_PUB , \
+  EMACS_PROJ_GIT , EMACS_PROJ_SWITCH , EMACS_PROJ_FILE , EMACS_PROJ_SEARCH , EMACS_PROJ_COMPILE  , EMACS_PROJ_SHELL , EMACS_FLYC_CHECK , EMACS_FLYC_NEXT , EMACS_FLYC_PREV , EMACS_FLYC_LIST , XXXXXXX     , XXXXXXX , \
+  XXXXXXX        , XXXXXXX           , XXXXXXX         , XXXXXXX           , EMACS_SWITCH_BUFFER , XXXXXXX          , XXXXXXX          , EMACS_FIND_FILE , XXXXXXX         , XXXXXXX         , XXXXXXX     , XXXXXXX , \
+  _______        , _______           , XXXXXXX         , XXXXXXX           , _______             , XXXXXXX          , XXXXXXX          , _______         , XXXXXXX         , XXXXXXX         , XXXXXXX     , XXXXXXX \
  )               ,
 
 /* Number Pad
@@ -588,6 +590,18 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   case EMACS_FLYC_LIST:
     if (record->event.pressed) {
       SEND_STRING(SS_LCTRL("c")"!l");
+    }
+    return true;
+    break;
+  case EMACS_FIND_FILE:
+    if (record->event.pressed) {
+      SEND_STRING(SS_LCTRL("x")"f");
+    }
+    return true;
+    break;
+  case EMACS_SWITCH_BUFFER:
+    if (record->event.pressed) {
+      SEND_STRING(SS_LCTRL("x")"b");
     }
     return true;
     break;
