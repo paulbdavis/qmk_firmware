6a0aa97505432c54bcdf06ed9e6a9991ffc6a12d
paulbdavis <paul@dangersalad.com>
2018-08-14 11:59:30 -0600

    trying...

--- users/dangersalad/dangersalad.c
+++ users/dangersalad/dangersalad.c
@@ -56,23 +56,23 @@ void layer_off_update_tri_layer(uint8_t layer_off, uint8_t layer1, uint8_t layer
 uint32_t layer_state_set_user(uint32_t state) {
 #ifdef RGBLIGHT_ENABLE
   switch (biton32(state)) {
-  /* case _RAISE: */
-  /*   rgblight_sethsv_orange(); */
-  /*   break; */
-  /* case _LOWER: */
-  /*   rgblight_sethsv_azure(); */
-  /*   break; */
+  case _RAISE:
+    rgblight_sethsv_noeeprom_orange();
+    break;
+  case _LOWER:
+    rgblight_sethsv_noeeprom_azure();
+    break;
   case _ADJUST:
-    rgblight_sethsv_white();
+    rgblight_sethsv_noeeprom_white();
     break;
   case _NUMPAD:
-    rgblight_sethsv_red();
+    rgblight_sethsv_noeeprom_red();
     break;
-  /* case _EMACS: */
-  /*   rgblight_sethsv_magenta(); */
-  /*   break; */
+  case _EMACS:
+    rgblight_sethsv_noeeprom_magenta();
+    break;
   default:
-    rgblight_sethsv_green();
+    rgblight_sethsv_noeeprom_green();
     break;
   }
 #ifdef SPLIT_KEYBOARD
