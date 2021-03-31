333a2e3c0c58c0e1e18a61af69eb4c97092add38
paulbdavis <paul@dangersalad.com>
2021-03-31 15:43:12 -0600

    nyquist added

---
+++ keyboards/keebio/nyquist/keymaps/dangersalad/config.h
@@ -0,0 +1,29 @@
+/*
+This is the c configuration file for the keymap
+
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+/* #define RGBLIGHT_LED_MAP { 1, 0, 2, 3, 5, 4, 10, 7, 6, 8, 9, 11 } */
+#define RGBLIGHT_LED_MAP { 0, 1, 2, 3, 4, 5, 11, 6, 7, 8, 9, 10 }
+
+/* #define RGBLIGHT_EFFECT_KNIGHT_LED_NUM 6 */
+
+#define BOOTMAGIC_KEY_SALT KC_RALT
+#define BOOTMAGIC_KEY_SWAP_RALT_RGUI KC_SPACE
