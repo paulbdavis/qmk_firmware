361a12857e07f929d50ce2e1037d6c59fb50a7f8
paulbdavis <paul@dangersalad.com>
2022-05-13 12:21:01 -0600

    iris makefile rules

--- keyboards/keebio/iris/keymaps/dangersalad/rules.mk
+++ keyboards/keebio/iris/keymaps/dangersalad/rules.mk
@@ -1,1 +1,17 @@
-RGBLIGHT_ENABLE = yes
+EXTRAFLAGS += -flto
+
+BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = no        # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = no            # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
+AUDIO_ENABLE = no           # Audio output
+SPLIT_KEYBOARD = yes
+ENCODER_ENABLE = yes
+RGB_MATRIX_ENABLE = no
+RGB_MATRIX_DRIVER = WS2812
+
+
